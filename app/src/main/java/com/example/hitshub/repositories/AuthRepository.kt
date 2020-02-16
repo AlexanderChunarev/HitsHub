eeaac45a58f8daf7d5ec12f03ed9b9a8f5b9f90a
AlexanderChunarev <alexanderchunarov1999@gmail.com>
2020-02-16 02:08:07 +0200

    created repos implementation

---
+++ app/src/main/java/com/example/hitshub/repositories/AuthRepository.kt
@@ -0,0 +1,40 @@
+package com.example.hitshub.repositories
+
+import androidx.lifecycle.MutableLiveData
+import com.example.hitshub.models.User
+import com.google.firebase.auth.AuthCredential
+
+class AuthRepository : BaseRepository() {
+    val userData by lazy { MutableLiveData<User>() }
+
+    fun createNewUser(authenticatedUser: User) {
+        val uidRef = userReference.document(authenticatedUser.uId)
+        uidRef.get().addOnSuccessListener {
+            if (!it!!.exists()) {
+                uidRef.set(authenticatedUser).addOnSuccessListener {
+                    authenticatedUser.isCreated = true
+                    userData.value = authenticatedUser
+                }
+            }
+        }
+    }
+
+    fun singInWithGoogle(authCredential: AuthCredential) {
+        val user = User()
+        firebaseAuth.signInWithCredential(authCredential).addOnSuccessListener {
+            val isNewUser = it.additionalUserInfo!!.isNewUser
+            firebaseAuth.currentUser.apply {
+                if (this != null) {
+                    user.apply {
+                        uId = uid
+                        name = displayName.toString()
+                        email = getEmail().toString()
+                        avatarUrl = photoUrl.toString()
+                        isNew = isNewUser
+                    }
+                    userData.value = user
+                }
+            }
+        }
+    }
+}
