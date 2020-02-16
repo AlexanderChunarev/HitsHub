ded34fbc3edd8bd4d65769f1c3f158bb9fe16766
AlexanderChunarev <alexanderchunarov1999@gmail.com>
2020-02-16 02:09:57 +0200

    created viewmodels implementation

--- app/src/main/java/com/example/hitshub/viewmodels/SplashViewModel.kt
+++ app/src/main/java/com/example/hitshub/viewmodels/SplashViewModel.kt
@@ -1,3 +1,21 @@
 package com.example.hitshub.viewmodels
 
-class SplashViewModel : BaseViewModel()
+import androidx.lifecycle.LiveData
+import com.example.hitshub.models.User
+import com.example.hitshub.repositories.SplashRepository
+
+class SplashViewModel : BaseViewModel() {
+    private val splashRepository by lazy { SplashRepository() }
+    lateinit var userAuntificationInfoLiveData: LiveData<User>
+    lateinit var userLiveData: LiveData<User>
+
+    fun setUserAuntificationInfo() {
+        splashRepository.checkUserFirebaseAuth()
+        userAuntificationInfoLiveData = splashRepository.userAuntificationData
+    }
+
+    fun setUserByUid(id: String) {
+        splashRepository.fetchUser(id)
+        userLiveData = splashRepository.fetchedUserData
+    }
+}
