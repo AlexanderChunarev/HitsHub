8daaada781e457b4771ba94936c447c1219dbe11
AlexanderChunarev <alexanderchunarov1999@gmail.com>
2020-02-16 13:31:02 +0200

    refactored code

--- app/src/main/java/com/example/hitshub/activities/RegisterActivity.kt
+++ app/src/main/java/com/example/hitshub/activities/RegisterActivity.kt
@@ -1,13 +1,21 @@
 package com.example.hitshub.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.hitshub.R
+import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        create_account_button.setOnClickListener {
+            startActivity(Intent(this, SignUpActivity::class.java))
+        }
+
+        have_account_button.setOnClickListener {}
     }
 }
--- app/src/main/java/com/example/hitshub/activities/SignUpActivity.kt
+++ app/src/main/java/com/example/hitshub/activities/SignUpActivity.kt
@@ -10,6 +10,7 @@ import com.example.hitshub.models.User
 import com.example.hitshub.utils.GoogleSignInHelper
 import com.example.hitshub.viewmodels.RegisterViewModel
 import com.google.firebase.auth.AuthCredential
+import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class SignUpActivity : BaseActivity() {
     private val googleSignInHelper by lazy { GoogleSignInHelper.getInstance() }
@@ -20,12 +21,14 @@ class SignUpActivity : BaseActivity() {
         googleSignInHelper.initGoogleSingInClient(this)
         setContentView(R.layout.activity_sign_up)
 
-        // call signIn () on button click
+        sign_up_with_google_button.setOnClickListener {
+            signIn()
+        }
     }
 
-    private fun singIn() {
-        val singInIntent = googleSignInHelper.googleSingInClient.signInIntent
-        startActivityForResult(singInIntent, SING_IN_CODE)
+    private fun signIn() {
+        val signInIntent = googleSignInHelper.googleSingInClient.signInIntent
+        startActivityForResult(signInIntent, SING_IN_CODE)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
