1f4f846ff5f2f11954ee5705e856410fc88403a9
AlexanderChunarev <alexanderchunarov1999@gmail.com>
2020-03-20 00:37:54 +0200

    created search menu

--- app/src/main/java/com/example/hitshub/fragments/SearchFragment.kt
+++ app/src/main/java/com/example/hitshub/fragments/SearchFragment.kt
@@ -1,14 +1,17 @@
 package com.example.hitshub.fragments
 
 import android.os.Bundle
+import android.view.*
+import android.widget.SearchView
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-
 import com.example.hitshub.R
 
 class SearchFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -17,5 +20,23 @@ class SearchFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_search, container, false)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.search_fragment_menu, menu)
+        val searchView = menu.findItem(R.id.search_view_btn).actionView as SearchView
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                // search by tracks/albums in web
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return false
+            }
+        })
+
+        super.onCreateOptionsMenu(menu, inflater)
     }
 }
